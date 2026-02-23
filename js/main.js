2f52102d4c80be54d04f8cff5db210300dbbd183
terabe1010 <terabehirokazu@gmail.com>
2026-02-23 09:10:05 +0900

    JS更新

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
 $(function () {
-
-  /*=================================================
-  ドロップダウンメニュー
-  ===================================================
-  */
-  $('.has-child-01').on('click', function () {
-    if ($('.has-child-01').hasClass('active-01')) {
-      $('.has-child-01').removeClass('active-01');
-    } else {
-      $('.has-child-01').addClass('active-01');
-    }
-  });
-  $('.has-child-02').on('click', function () {
-    if ($('.has-child-02').hasClass('active-02')) {
-      $('.has-child-02').removeClass('active-02');
-    } else {
-      $('.has-child-02').addClass('active-02');
-    }
-  });
-  $('.has-child-03').on('click', function () {
-    if ($('.has-child-03').hasClass('active-03')) {
-      $('.has-child-03').removeClass('active-03');
-    } else {
-      $('.has-child-03').addClass('active-03');
-    }
-  });
-
   /*=================================================
   ハンバーガーメニュー
   ===================================================*/
@@ -50,52 +23,6 @@ $(function () {
     $('#header').removeClass('open');
   });
 
-
-  /*=================================================
-  アコーディオン
-  ===================================================*/
-  $('.title.a-title').on('click', function () {//タイトル要素をクリックしたら
-    var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
-    $(findElm).slideToggle();//アコーディオンの上下動作
-
-    if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
-      $(this).removeClass('close');//クラス名を除去し
-    } else {//それ以外は
-      $(this).addClass('close');//クラス名closeを付与
-    }
-  });
-
-  /*=================================================
-  トップに戻る
-  ===================================================*/
-  let pagetop = $('#to-top');
-  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
-  pagetop.hide();
-
-  // スクロールイベント（スクロールされた際に実行）
-  $(window).scroll(function () {
-    // スクロール位置が700pxを超えた場合
-    if ($(this).scrollTop() > 700) {
-      // トップに戻るボタンを表示する
-      pagetop.fadeIn();
-
-      // スクロール位置が700px未満の場合
-    } else {
-      // トップに戻るボタンを非表示にする
-      pagetop.fadeOut();
-    }
-  });
-
-  // クリックイベント（ボタンがクリックされた際に実行）
-  pagetop.click(function () {
-    // 0.5秒かけてページトップへ移動
-    $('body,html').animate({ scrollTop: 0 }, 500);
-
-    // イベントが親要素へ伝播しないための記述
-    // ※詳しく知りたい方は「イベント　バブリング」または「jQuery バブリング」で調べてみてください
-    return false;
-  });
-
   /*=================================================
   フェード
   ===================================================*/
@@ -111,17 +38,17 @@ $(function () {
   スムーススクロール
   ===================================================*/
   $('a[href^="#"]').click(function () {
-    // スクロールの速度
     let speed = 1500;
-    // スクロールタイプ
     let type = 'swing';
-    // href属性の取得
+
     let href = $(this).attr("href");
-    // 移動先の取得（hrefが#indexならトップ$(html)に、）
     let target = $(href == "#index" ? 'html' : href);
-    // 移動先のポジション取得
-    let position = target.offset().top;
-    // animateでスムーススクロール
+
+    // 画面幅でヘッダー高さを切り替え
+    let headerHeight = $(window).width() <= 768 ? 60 : 80;
+
+    let position = target.offset().top - headerHeight;
+
     $('body,html').animate({ scrollTop: position }, speed, type);
     return false;
   });
